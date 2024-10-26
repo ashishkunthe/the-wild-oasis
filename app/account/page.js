0bcc39e81fd53c3a4312277e3585b11535562a53
ashishkunthe <ashishkunthe2003@gmail.com>
2024-10-26 20:59:28 +0530

    first commit

---
+++ app/account/page.js
@@ -0,0 +1,11 @@
+import Navigation from "../components/Navigation";
+
+function Page() {
+  return (
+    <div>
+      <h3>Your account</h3>
+    </div>
+  );
+}
+
+export default Page;
---
+++ app/cabins/page.js
@@ -0,0 +1,11 @@
+import Navigation from "../components/Navigation";
+
+function Page() {
+  return (
+    <div>
+      <h1>Cabins page</h1>
+    </div>
+  );
+}
+
+export default Page;
